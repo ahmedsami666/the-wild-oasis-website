ad5fcf21cb17273633064c755ec4723450e77c29
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-10-10 12:39:28 +0300

    adding middleware function

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,10 @@
-export default function page() {
+import { auth } from "../_lib/auth"
+
+export default async function page() {
+    const session = await auth()
     return (
         <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-            Welcome,
+            Welcome, {session?.user?.name}
         </h2>
     )
 }
