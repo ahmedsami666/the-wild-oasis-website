173fd882ac04a378f72c9e5b8adbcaa34da89cdd
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-10-13 13:44:37 +0300

    allow users to delete booking

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache"
 import { auth, signIn, signOut } from "./auth"
 import { supabase } from "./supabase"
+import { getBookings } from "./data-service"
 
 export async function signInAction() {
     await signIn('google', 
@@ -39,4 +40,23 @@ export async function updateGuest(formData){
     // revalidate data 
     revalidatePath('/account/profile')
     return data;
+}
+export async function deleteReservation(bookingId) {
+    const session = await auth()
+    if (!session) throw new Error('you must be logged in')
+    const guestBookings = await getBookings(session.user.guestId)
+    const guestBookingIds = guestBookings.map((booking) => booking.id)
+
+    if (!guestBookingIds.includes(bookingId)) throw new Error('you are not allowed to delete this booking')
+
+    const { data, error } = await supabase
+    .from('bookings')
+    .delete()
+    .eq('id', bookingId);
+
+    if (error) {
+      console.error(error);
+      throw new Error('Booking could not be deleted');
+    }
+    revalidatePath('account/reservations')
 }
